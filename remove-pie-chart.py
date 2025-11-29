4439fcd2871e409343b6e125240ac331d718652d
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2025-11-30 06:23:17 +0900

    Final: Working pie chart removal with both fixes

--- remove-pie-chart.py
+++ remove-pie-chart.py
@@ -6,6 +6,9 @@ svg_file = "profile-3d-contrib/profile-night-rainbow.svg"
 with open(svg_file, 'r') as f:
     content = f.read()
 
+# translate(130, 130)"></g></g> を translate(130, 130)"></g> に修正
+content = content.replace('translate(130, 130)"></g></g>', 'translate(130, 130)"></g>')
+
 # 空の<g transform="translate(40, 520)">...</g>を削除
 pattern = r'<g transform="translate\(40, 520\)">(?:<g[^>]*></g>)*</g>'
 content = re.sub(pattern, '', content)
